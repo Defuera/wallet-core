3aa338ef809be8a291c29ca52f92bfa02343b3ec
hewigovens <360470+hewigovens@users.noreply.github.com>
2019-05-10 11:53:18 +0800

    Add derive tests and update block explorer

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/CoinAddressDerivationTests.kt
@@ -70,7 +70,7 @@ class CoinAddressDerivationTests {
         STEEM -> assertEquals("STM6WSusVTidc1e7TbLjhqQPYctbsndTRwXHpi82gMuJyKEkJVLvg", address)
         EOS -> assertEquals("EOS6hs8sRvGSzuQtq223zwJipMzqTJpXUVjyvHPvPwBSZWWrJTJkg", address)
         IOTEX -> assertEquals("io1qw9cccecw09q7p5kzyqtuhfhvah2mhfrc84jfk", address)
-        ZILLIQA -> assertEquals("0xddb41006f7b6fa8e5fbf06a71c01f789febc66e8", address)
+        ZILLIQA -> assertEquals("0xDdb41006F7B6FA8e5FBF06A71c01F789FeBC66e8", address)
         SEMUX -> assertEquals("0xfe604170382452f77bc922bc19eb4b53504b09c2", address)
     }
 }
