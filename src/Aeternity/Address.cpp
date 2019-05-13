b301488be84a5ebeb60dbabd069c97905d94e2b7
defuera <1181883+Defuera@users.noreply.github.com>
2019-05-13 21:59:48 +0200

    [resolves #260] implement Address::isValid function + tests

--- src/Aeternity/Address.cpp
+++ src/Aeternity/Address.cpp
@@ -13,25 +13,12 @@ using namespace TW::Aeternity;
 /// Determines whether a string makes a valid address.
 /// copied from \see https://github.com/aeternity/aepp-sdk-js/blob/develop/es/utils/crypto.js
 bool TW::Aeternity::Address::isValid(const std::string &string) {
-    const std::string &type = assertedType(string, prefix);
-    unsigned long base58 = Base58::bitcoin.decodeCheck(type).size();
-    return base58 == 32;
-}
-
-std::string Address::assertedType(const std::string &data, const std::string &type) {
-    const std::basic_regex base_regex("^${ak\\_}_.+ $");
-    std::smatch base_match;
-
-    if (std::regex_match(data, base_regex)) {
-
-        int n = data.length();
-        char charBuffer[n + 1];
-        strcpy(charBuffer, data.c_str());
-
-        char *token = strtok(charBuffer, "_");
-        return token;
+    if (string.empty()) {
+        return false;
     } else {
-        return nullptr;
+        auto type = string.substr(0, prefix.size());
+        auto payload = string.substr(prefix.size(), string.size() - 1);
+        return checkType(type) && checkPayload(payload);
     }
 }
 
@@ -54,4 +41,11 @@ TW::Aeternity::Address::Address(const PublicKey &publicKey) {
 /// Returns a string representation of the Bravo address.
 std::string Address::string() const {
     return ""; // prefix + Base58::bitcoin.encodeCheck("");
+}
+bool Address::checkType(const std::string &type) {
+    return type == prefix;
+}
+bool Address::checkPayload(const std::string &payload) {
+    unsigned long base58 = Base58::bitcoin.decodeCheck(payload).size();
+    return base58 == 32;
 }
--- src/Aeternity/Address.h
+++ src/Aeternity/Address.h
@@ -6,29 +6,27 @@
 
 #include <PublicKey.h>
 #include <string>
-#include <regex>
 
 namespace TW::Aeternity {
 
-    static const std::string prefix = "ak_"; //todo should I use 'ak_'?
+static const std::string prefix = "ak_";
 
 class Address {
   public:
-
     /// Determines whether a string makes a valid address.
-    static bool isValid(const std::string& string);
+    static bool isValid(const std::string &string);
 
     /// Initializes an address from a string representation.
-    Address(const std::string& string);
+    explicit Address(const std::string &string);
 
     /// Initializes an address from a public key.
-    Address(const PublicKey& publicKey);
+    explicit Address(const PublicKey &publicKey);
 
     /// Returns a string representation of the Aeternity address.
     std::string string() const;
 
   private:
-    static std::string assertedType(const std::string& data, const std::string& type);
-    std::regex* regex;
+    static bool checkType(const std::string& type);
+    static bool checkPayload(const std::string& payload);
 };
 } // namespace TW::Aeternity
